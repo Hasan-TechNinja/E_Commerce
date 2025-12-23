f7b3afc1d68ea8f59636f61581b29813750fd9e5
Hasan-TechNinja <hasantechninja@gmail.com>
2025-12-23 15:05:40 +0600

    OTP table hide from admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,19 +3,6 @@ from . models import EmailVerification, PasswordResetCode, Profile
 
 # Register your models here.
 
-class EmailVerificationAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'user', 'code', 'created_at'
-    )
-admin.site.register(EmailVerification, EmailVerificationAdmin)
-
-
-
-class PasswordResetCodeAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'user', 'code', 'created_at'
-    )
-admin.site.register(PasswordResetCode, PasswordResetCodeAdmin)
 
 
 class ProfileAdmin(admin.ModelAdmin):
